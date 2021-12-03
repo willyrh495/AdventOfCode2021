1e1b46c0af84101c15792427927dc09ddab81286
Willy Revilla <willy@b89.io>
2021-12-02 22:50:02 -0500

    day2

--- app/src/main/java/com/willy/android/codeofadvent2021/Day2.kt
+++ app/src/main/java/com/willy/android/codeofadvent2021/Day2.kt
@@ -1,4 +1,31 @@
 package com.willy.android.codeofadvent2021
 
+import com.willy.android.codeofadvent2021.utils.ReadFile
+
 class Day2 {
+
+    fun calculateTrajectory() {
+        val data = ReadFile.read("src/day2sample").split("\n")
+        var horizontalPosition = 0
+        var depth = 0
+        var aim = 0
+
+        data.forEach {
+            val direction = it.split(" ")
+
+            when (direction.first()) {
+                "forward" -> {
+                    horizontalPosition += direction.last().toInt()
+                    depth += aim * direction.last().toInt()
+                }
+                "down" -> {
+                    aim += direction.last().toInt()
+                }
+                "up" -> {
+                    aim -= direction.last().toInt()
+                }
+            }
+        }
+        println(horizontalPosition * depth)
+    }
 }
--- app/src/test/java/com/willy/android/codeofadvent2021/Day2Test.kt
+++ app/src/test/java/com/willy/android/codeofadvent2021/Day2Test.kt
@@ -1,4 +1,12 @@
 package com.willy.android.codeofadvent2021
 
+import org.junit.Test
+
 class Day2Test {
+
+    @Test
+    fun test() {
+        val day2 = Day2()
+        day2.calculateTrajectory()
+    }
 }
